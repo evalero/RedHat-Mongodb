ea39d1a79ad3a86101a26e91d756d47d00d6b0a8
email <evalero@paradigmadigital.com>
2016-04-12 22:59:12 +0200

    Cambio de estructura de directorios
    Aniadido el directorio de anexos de datos

--- Tema5/Replicaset/startRS.sh
+++ Tema5/Replicaset/startRS.sh
@@ -5,13 +5,13 @@ mkdir -p /data/RS1_2
 mkdir -p /data/RS1_3
 
 #Start shard servers
-mongod --replSet RS1 --dbpath /data/RS1_1 --port 27017 --noprealloc --nojournal &
-mongod --replSet RS1 --dbpath /data/RS1_2 --port 27018 --noprealloc --nojournal &
-mongod --replSet RS1 --dbpath /data/RS1_3 --port 27019 --noprealloc --nojournal &
+mongod --replSet RS1 --dbpath /data/RS1_1 --port 27017 --noprealloc --nojournal --logpath /data/RS1_1/mongod.log &
+mongod --replSet RS1 --dbpath /data/RS1_2 --port 27018 --noprealloc --nojournal --logpath /data/RS1_2/mongod.log &
+mongod --replSet RS1 --dbpath /data/RS1_3 --port 27019 --noprealloc --nojournal --logpath /data/RS1_3/mongod.log &
 
 #initiate replica set for each shard
 sleep 2
-mongo  createRS1.js 
+mongo  createRS1.js
 #Populate replication database
 sleep 10
 mongo  populateDB.js
